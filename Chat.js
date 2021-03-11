3966012ad309d7738f9a25d83e2da985e4d1ddaf
luiscostalafc <luiscostalafc@gmail.com>
2021-03-10 21:34:39 -0300

    feat: finished Chat Component

--- Chat.js
+++ Chat.js
@@ -6,30 +6,41 @@ export const Chat = () => {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello developer',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-    ]);
+    let client = new Parse.LiveQueryClient({
+      applicationId: 'bbu7p4atzxhrVg4o1l9ukhUthwY6QmgFm2Jc93Fu',
+      serverURL: 'wss://' + 'reactnativechat.b4a.io',
+      javascriptKey: 'xQpQqYUPuduHFf8epuk3EJAInrDaBYFrVI5a08dl',
+    });
+    client.open();
+
+    let query = new Parse.Query('Messages');
+    query.ascending('createdAt');
+    query.limit(1);
+    let subscription = client.subscribe(query);
+
+    subscription.on('create', (messageParse) => {
+      setMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, {
+          _id: messageParse.id,
+          text: messageParse.get('message'),
+          createdAt: new Date(),
+          user: {
+            _id: '12345',
+            name: 'Back4app',
+            avatar: 'https://i.pravatar.cc/300',
+          },
+        }))
+    })
   }, []);
 
   const onSend = useCallback((messages = []) => {
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, messages),
-    );
-    const Message = Parse.Object.extend('Message');
-    let message = new Message();
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
 
-    message.set('content', messages[0].text);
-    message.save();
-  }, []);
+    const Message = Parse.Object.extend("Message");
+    const thisMessage = new Message();
+    thisMessage.set("content", messages[0].text);
+    thisMessage.save()
+  }, [])
 
   return (
     <GiftedChat
